3f354d3a86b80cc61bbfac655226decbe72a4a6f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-05 21:35:53 +0700

    update-Add-About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          OverView
+        </h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a skilled fullstack developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, ExpressJs and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+    </>
   )
 }
 
