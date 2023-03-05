cafb36bfdd3d928f0820f4637a73206c099d9ac3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-05 17:27:18 +0700

    update-MenuSearch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,8 +46,27 @@ const Navbar = () => {
 
           {/*======================== Searchbar========================= */}
 
-          
+          <div className="sm:block hidden justify-center items-center">
+            <div className="relative mb-3 xl:w-96" data-te-input-wrapper-init>
+              <input
+                type="search"
+                className="peer block min-h-[auto]
+                  w-full rounded-2xl border-2 bg-red-500
+                  py-[0.32rem] px-3 leading-[1.6]
+                  outline-none transition-all
+                  duration-200 ease-linear
+                  focus:placeholder:opacity-0
+                  data-[te-input-state-active]:placeholder:opacity-100
+                  motion-reduce:transition-none
+                  dark:text-neutral-200
+                  dark:placeholder:text-white
+                  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="exampleSearch2"
+                placeholder="Search..."
+              />
 
+            </div>
+          </div>
 
           {/* ===========================LinkMap========================= */}
           <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -61,7 +80,9 @@ const Navbar = () => {
                   } hover:text-white text-[18px]
                     font-medium cursor-pointe
                     transition-all duration-200
-                    ease-in-out`
+                    ease-in-out border-2 py-1 px-4
+                    border-slate-800
+                    shadow-zinc-400 rounded-3xl`
                 }
                 onClick={() => setActive(nav.title)}
               >
@@ -73,7 +94,7 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-
+          {/* ===================SubMenu============================== */}
           <div className="sm:hidden flex flex-1 justify-end items-center">
               <img
                 src={toggle ? close : menu}
@@ -94,13 +115,38 @@ const Navbar = () => {
                   ease-in-out
                 `
               }>
-
+                <ul
+                  className='list-none flex justify-end
+                  items-start flex-col gap-4'
+                >
+                  {navLinks.map((nav) => (
+                    <li
+                      key={nav.id}
+                      className={`${
+                          active === nav.title
+                          ? "text-white"
+                          : "text-secondary"
+                        } hover:text-white text-[18px]
+                          font-medium cursor-pointe
+                          transition-all duration-200
+                          ease-in-out`
+                      }
+                      onClick={() => setActive(nav.title)}
+                    >
+                      <a
+                        href={`#${nav.id}`}
+                      >
+                        {nav.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
               </div>
           </div>
         </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
