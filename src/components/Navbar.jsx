e3af4b7a8b54669e1bf801d73497077e1d54c3a2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-05 11:01:48 +0700

    update-Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+
+  const [active, setActive] = useState(" ");
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <>
+      <nav
+        className={`${styles.paddingX}
+          w-full flex items-center
+          py-5 fixed top-0 z-20 bg-primary`
+        }
+      >
+        <div
+          className="w-full flex justify-between
+          items-center max-w-7xl mx-auto"
+        >
+          <Link
+            to="/"
+            className='flex items-center gap-2'
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img
+              src={logo}
+              alt="logo"
+              className="w-9 h-9 object-contain"
+            />
+            <p
+              className='text-white text-[18px]
+              font-bold cursor-pointer'
+            >
+              BaoNguyen
+              <span className="sm:block hidden">
+                | FullstackDev.IO
+              </span>
+            </p>
+          </Link>
+          {/* ===========================LinkMap========================= */}
+          <ul className='list-none hidden sm:flex flex-row gap-10'>
+            {navLinks.map((nav) => (
+              <li
+                key={nav.id}
+                className={`${
+                    active === nav.title
+                    ? "text-white"
+                    : "text-secondary"
+                  } hover:text-white text-[18px]
+                    font-medium cursor-pointe
+                    transition-all duration-200
+                    ease-in-out`
+                }
+                onClick={() => setActive(nav.title)}
+              >
+                <a
+                  href={`#${nav.id}`}
+                >
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+              <img
+                src={toggle ? close : menu}
+                alt='Menu'
+                className='w-[28px] h-[28px]
+                  object-contain cursor-pointer
+                  transition-all duration-200
+                  ease-in-out'
+                onClick={() => setToggle(!toggle)}
+              />
+
+              <div className={`
+                  ${!toggle ? 'hidden' : 'flex'}
+                  p-6 black-gradient absolute
+                  top-20 right-0 mx-4 my-2
+                  min-w-[140px] z-10 rounded-xl
+                  transition-all duration-300
+                  ease-in-out
+                `
+              }>
+
+              </div>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
 
